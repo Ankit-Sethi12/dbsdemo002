b62f3e29f71a6657e28087bec71efeac12f7a6f2
Ankit-Sethi12 <32670860+Ankit-Sethi12@users.noreply.github.com>
2017-10-10 12:44:22 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 echo "<h1>Hello World</h1>";
 echo "<p>Hello world, this is me</p>";
-echo "<p>This is new paragraph</p>";
+echo "<p>This is a new paragraph</p>";
+echo "<p>How are you?</p>";
 
